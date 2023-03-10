414553e7cfd9582f504b68de1bc04242d81cb273
Alan Tran-Kiem <alan001tk@gmail.com>
2023-03-09 23:04:15 -0800

    Remove trivial code

---
+++ oreilly_book/cli_parse/target/debug/deps/cli_parse-bf2c81b2ea466549.d
@@ -0,0 +1,5 @@
+/home/alan/projects/rust_learning/oreilly_book/cli_parse/target/debug/deps/cli_parse-bf2c81b2ea466549.rmeta: src/main.rs
+
+/home/alan/projects/rust_learning/oreilly_book/cli_parse/target/debug/deps/cli_parse-bf2c81b2ea466549.d: src/main.rs
+
+src/main.rs:
---
+++ oreilly_book/cli_parse/target/debug/deps/cli_parse-d801f974dfa09de1.d
@@ -0,0 +1,5 @@
+/home/alan/projects/rust_learning/oreilly_book/cli_parse/target/debug/deps/cli_parse-d801f974dfa09de1.rmeta: src/main.rs
+
+/home/alan/projects/rust_learning/oreilly_book/cli_parse/target/debug/deps/cli_parse-d801f974dfa09de1.d: src/main.rs
+
+src/main.rs:
